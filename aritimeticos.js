066d8be738b797e85ea87580d6595f55f3a5ae6f
Stefano <stefanopaulo47@gmail.com>
2019-06-07 15:12:33 -0300

    operadores: aritimeticos

---
+++ aritimeticos.js
@@ -0,0 +1,12 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+console.log(soma)
+const sub = d - b
+console.log(sub)
+const mult = a * b
+console.log(mult)
+const div = d / a
+console.log(div)
+const modulo = a % b
+console.log(-modulo)

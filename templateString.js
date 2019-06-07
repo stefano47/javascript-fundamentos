211c29d31008140e8127d46602edd60728acb575
Stefano <stefanopaulo47@gmail.com>
2019-06-07 12:08:36 -0300

    template string

---
+++ templateString.js
@@ -0,0 +1,11 @@
+const nome = 'Rebeca'
+const concatenacao = `
+    Olá 
+    ${nome}!`
+console.log(concatenacao)
+
+// espressões...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`) 

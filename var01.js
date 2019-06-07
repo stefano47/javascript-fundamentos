1dfcdacbc682a7a6990035491a73493f9aa34248
Stefano <stefanopaulo47@gmail.com>
2019-06-07 13:49:54 -0300

    declaracao de variaveis com var

---
+++ var01.js
@@ -0,0 +1,22 @@
+// Declaração de váriaveis com var #01
+
+{
+    {
+        {
+            { 
+                var sera = 'Será???' 
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+teste()
+// console.log(local)
+// uma váriavel fora de uma função é uma váriavel global
---
+++ var02.js
@@ -0,0 +1,10 @@
+// Declaração de váriaveis com var #02
+
+var numero = 1
+
+{
+    var numero = 2
+    console.log('dentro = ',numero) // sobrescreve o valor
+}
+
+console.log('fora = ',numero)
